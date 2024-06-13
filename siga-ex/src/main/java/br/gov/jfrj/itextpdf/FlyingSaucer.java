bae81b95df8f813174061704b7b64242ce7cd667
Luizhoa03 <lhoa4535@gmail.com>
2024-06-13 13:17:56 -0300

    CorrecaoFont1

--- siga-ex/src/main/java/br/gov/jfrj/itextpdf/FlyingSaucer.java
+++ siga-ex/src/main/java/br/gov/jfrj/itextpdf/FlyingSaucer.java
@@ -307,7 +307,7 @@ public class FlyingSaucer implements ConversorHtml {
 	    // A expressão regular abaixo procura por 'font-family:' seguido por qualquer
 	    // combinação de espaços e 'AvantGarde Bk BT' ou 'Arial', possivelmente
 	    // misturados com outras fontes, e substitui.
-	    html = html.replaceAll("font-family:.*?(AvantGarde Bk BT|Arial)", "font-family: Open Sans");
+	    html = html.replaceAll("font-family:.*?(serif)", "font-family: Arial");
 	    return html;
 	}
 
