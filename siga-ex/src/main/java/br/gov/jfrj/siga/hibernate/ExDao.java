2cc3da1d9f60df88f0b00a30ab8c94477e2cccee
Gabriel Barros <ktbarros07@gmail.com>
2025-11-10 12:18:27 -0300

    fix: assert schema para restrição à eliminação

--- siga-ex/src/main/java/br/gov/jfrj/siga/hibernate/ExDao.java
+++ siga-ex/src/main/java/br/gov/jfrj/siga/hibernate/ExDao.java
@@ -2557,12 +2557,15 @@ public class ExDao extends CpDao {
 		return docDeletedCount;
 	}
 
-	public List<Long> listarIdsUsuariosPermitidosEliminacao(){
-		Query query = em()
-			.createNativeQuery("SELECT c.ID_PESSOA " + 
-				"FROM cp_configuracao c " + 
-				"JOIN cp_grupo g ON g.ID_GRUPO = c.ID_GRUPO " + 
-				"WHERE c.ID_TP_CONFIGURACAO = 203 AND g.SIGLA_GRUPO = 'Eli' AND g.DESC_GRUPO = 'Eliminadores'; ");
+	public List<Long> listarIdsUsuariosPermitidosEliminacao() {
+		Query query = em().createNativeQuery(
+			"SELECT c.ID_PESSOA " +
+			"FROM corporativo.cp_configuracao c " +
+			"JOIN corporativo.cp_grupo g ON g.ID_GRUPO = c.ID_GRUPO " +
+			"WHERE c.ID_TP_CONFIGURACAO = 203 " +
+			"AND g.SIGLA_GRUPO = 'Eli' " +
+			"AND g.DESC_GRUPO = 'Eliminadores'"
+		);
 		return query.getResultList();
 	}
 	
